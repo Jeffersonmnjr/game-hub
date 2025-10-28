ecb18c735e8d25bfacf3b6c519d7129bf360484f
Jeffersonmnjr <jeffersonmnjr@gmail.com>
2025-10-28 04:56:52 -0300

    Display Genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width={"300px"} border={10} overflow={"hidden"}>
+    <Box borderRadius={10} overflow={"hidden"}>
       {children}
     </Box>
   );
