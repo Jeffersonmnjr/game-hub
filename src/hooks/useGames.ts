98e621abacb1d61fb744b159a93059051abed46b
Jeffersonmnjr <jeffersonmnjr@gmail.com>
2025-10-28 05:32:34 -0300

    Filter Games by Genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -16,8 +17,10 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = () => {
-  return useData<Game>("/games");
+const useGames = (selectedGenre: Genre | null) => {
+  return useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 };
 
 export default useGames;
